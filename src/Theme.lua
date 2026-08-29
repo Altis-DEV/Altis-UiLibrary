d4357584db3db554309b12c449b7414d8f98a3a8
Altis <flandrescripter@gmail.com>
2026-08-29 17:29:54 +0700

    Update Theme.lua

--- src/Theme.lua
+++ src/Theme.lua
@@ -1,1 +1,35 @@
+local Theme = {}
 
+--==================================================
+-- Default Theme
+--==================================================
+
+Theme.Default = {
+    Text = Color3.fromRGB(255, 255, 255),
+
+    Topbar = Color3.fromRGB(30, 30, 30),
+
+    TabContainer = Color3.fromRGB(25, 25, 25),
+
+    Background = Color3.fromRGB(20, 20, 20),
+
+    ResizeCorner = Color3.fromRGB(120, 120, 120),
+}
+
+--==================================================
+-- Light Theme
+--==================================================
+
+Theme.Light = {
+    Text = Color3.fromRGB(25, 25, 25),
+
+    Topbar = Color3.fromRGB(235, 235, 235),
+
+    TabContainer = Color3.fromRGB(220, 220, 220),
+
+    Background = Color3.fromRGB(245, 245, 245),
+
+    ResizeCorner = Color3.fromRGB(130, 130, 130),
+}
+
+return Theme
